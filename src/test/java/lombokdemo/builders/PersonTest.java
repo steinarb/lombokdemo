a53319ba226b2a631b15a698dc8e52bdc27a6cae
Steinar Bang <sb@dod.no>
2022-08-25 21:08:38 +0200

    Add test for the generated builder toString()

--- src/test/java/lombokdemo/builders/PersonTest.java
+++ src/test/java/lombokdemo/builders/PersonTest.java
@@ -1,5 +1,6 @@
 package lombokdemo.builders;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ class PersonTest {
             .build();
         assertEquals(name, bean.getName());
         assertEquals(age, bean.getAge());
+        assertThat(bean.toString()).startsWith(Person.class.getCanonicalName());
     }
 
     @Test
@@ -40,6 +42,20 @@ class PersonTest {
         var olderBean = bean.toBuilder().age(oldAge).build();
         assertEquals(name, olderBean.getName());
         assertEquals(oldAge, olderBean.getAge());
+    }
+
+    @Test
+    void testBuilderToString() {
+        String name = "John Doe";
+        Integer age = 42;
+        var builderAsString = Person.builder()
+            .name(name)
+            .age(age)
+            .toString();
+        assertThat(builderAsString)
+            .startsWith("Person.PersonBuilder")
+            .contains(name)
+            .contains(age.toString());
     }
 
 }
